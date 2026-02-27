97a69bb98469d595f2c8158fd2637365b15c9429
denis-axon <denis@axonops.com>
2026-02-27 10:51:16 +0200

    Create markdown.go with MarkdownToPDF function

---
+++ internal/converter/markdown.go
@@ -0,0 +1,24 @@
+package converter
+
+import (
+	"github.com/solworktech/md2pdf"
+	"io/ioutil"
+	"log"
+)
+
+// MarkdownToPDF converts a markdown file to a PDF
+func MarkdownToPDF(inputFile string, outputFile string) error {
+	// Read the markdown file
+	data, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	// Convert to PDF
+	err = md2pdf.Convert(data, outputFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
